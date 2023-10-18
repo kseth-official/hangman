f3462e5c3622964d45b7e8a80ba131b10b2f7955
Kush Sethi <kushagra.sethi.io@gmail.com>
2023-10-17 22:59:43 -0700

    Deleted comment from Hangman.cpp

--- Hangman.cpp
+++ Hangman.cpp
@@ -1,28 +1,3 @@
-/*A TEXT BASED IMPLEMENTATION OF HANGMAN
-Menu:
-1. Play Game
-2. Exit
-
-Play Game =>
-Rules:
-Game is designed for 2 players
- One person enters a word/phrase (which can include spaces and special characters) as the word/phrase to be guessed.
-//A screen appears with dashes indicating letters of the word/phrase (dash does not occur for spaces)
-The other person must either: a) type in a letter which he/she thinks exists b) choose to enter the entire word 
-If 6 wrong guesses are made for which letters could exist, the person loses the game.
-
-	*****
-	*	*
-	0	*
-   \|/  *
-	|	*
-   / \  *
-		*
-		*
-*********
-
-*/
-
 #include "Hangman.h"
 
 const char uparrow = 72;
